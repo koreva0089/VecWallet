2f6c012cdf1cc472ffa6d67722d51a8ce1992db2
koreva0089 <korevavitalij0089@gmail.com>
2025-11-05 22:14:54 +0200

    project creation

---
+++ app/src/main/java/com/example/vecwallet/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.vecwallet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.vecwallet.ui.VecWalletApp
+import com.example.vecwallet.ui.theme.VecWalletTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VecWalletTheme {
+                VecWalletApp()
+            }
+        }
+    }
+}
